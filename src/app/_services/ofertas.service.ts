3b9f3db7195fc3b65ac2a93fa3fb5efd63f11fa3
fabiokaspar <fabiokaspar@gmail.com>
2019-11-22 11:34:52 -0300

    Oferts service implements functionalities like saving, updating offer by advertiser user. Also it implements requests by filtered offers

--- src/app/_services/ofertas.service.ts
+++ src/app/_services/ofertas.service.ts
@@ -1,64 +1,88 @@
+import { AuthService } from './auth.service';
 import { Observable, throwError, of } from 'rxjs';
 import { environment } from './../../environments/environment';
 import { Oferta } from '../_models/oferta';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { retry, debounceTime, map, catchError, distinctUntilChanged, switchMap, distinct } from 'rxjs/operators';
+import { retry, catchError, map } from 'rxjs/operators';
 
 @Injectable()
 export class OfertasService {
   public ofertas: Oferta[];
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) {}
 
-  public getOfertasDestaque(): Promise<Oferta[]>
+  public getOfertasDestaque(): Observable<Oferta[]>
   {
-    return this.http.get<Oferta[]>(`${environment.URI_API}/ofertas?destaque=true`)
-      .toPromise()
-      .then((ofertas: Oferta[]) => ofertas);
+    let path: string = `${environment.URI_API}/ofertas`;
+
+    if (this.authService.isLoggedIn())
+    {
+      path += `/user/${this.authService.getDecodedToken().nameid}`;
+    }
+    return this.http.get<Oferta[]>(path);
   }
 
-  public getOfertasRestaurante(): Promise<Oferta[]>
+  public getOfertasRestaurante(): Observable<Oferta[]>
   {
-    return this.http.get(`${environment.URI_API}/ofertas?categoria=restaurante`)
-      .toPromise()
-      .then((ofertas: Oferta[]) => ofertas);
+    return this.http.get<Oferta[]>(`${environment.URI_API}/ofertas?categoria=restaurante`);
   }
 
-  public getOfertasDiversao(): Promise<Oferta[]>
+  public getOfertasDiversao(): Observable<Oferta[]>
   {
-    return this.http.get(`${environment.URI_API}/ofertas?categoria=diversao`)
-      .toPromise()
-      .then((ofertas: Oferta[]) => ofertas);
+    return this.http.get<Oferta[]>(`${environment.URI_API}/ofertas?categoria=diversao`);
   }
 
   public getOfertaDetail(id): Observable<Oferta> {
     return this.http.get<Oferta>(`${environment.URI_API}/ofertas/${id}`);
   }
 
-  public getComoUsarOferta(id): Promise<string> {
-    return this.http.get(`${environment.URI_API}/como-usar/${id}`)
-      .toPromise()
-      .then((response: any) => {
-        console.log(response)
-        return response.descricao;
-      });
-  }
-
-  public getOndeFicaOferta(id): Promise<string> {
-    return this.http.get(`${environment.URI_API}/onde-fica/${id}`)
-      .toPromise()
-      .then((response: any) => {
-        console.log(response)
-        return response.descricao;
-      });
-  }
-
   public pesquisaPorOfertas(query: string): Observable<Oferta[]>
   {
-    return this.http.get<Oferta[]>(`${environment.URI_API}/ofertas?descricao_like=${query}`).pipe(
+    return this.http.post<Oferta[]>(`${environment.URI_API}/ofertas/like`, query).pipe(
       retry(10)
     );
   }
 
+  public getOfertasProprietarias(offerParams?): Observable<Oferta[]>
+  {
+    let params = new HttpParams();
+
+    if (offerParams != null)
+    {
+      // tslint:disable-next-line: forin
+      for (const key in offerParams)
+      {
+        console.log(key + " => "+ offerParams[key]);
+        params = params.append(key, offerParams[key]);
+      }
+      console.log(params)
+    }
+
+    return this.http.get<Oferta[]>(`${environment.URI_API}/ofertas/filter`, { params } ).pipe(
+      map(response => {
+        console.log(response)
+        return response;
+      }),
+      catchError(error => {
+        console.log(error)
+        return throwError(error);
+      })
+    )
+  }
+
+  public salvaOferta(userId, novaOferta: Oferta): Observable<any>
+  {
+    return this.http.post(`${environment.URI_API}/ofertas/create?userId=${userId}`, novaOferta);
+  }
+
+  public updateOferta(userId, oferta): Observable<any>
+  {
+    return this.http.post(`${environment.URI_API}/ofertas/update?userId=${userId}`, oferta);
+  }
+
+  
 }
